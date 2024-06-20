5f16016aa11e7d3204a4f9bd1cc5ab1cf0ed1c8c
Manish9982 <manish998268@gmail.com>
2024-06-20 11:02:05 +0530

    LiveApp

--- src/assets/Schemes/Constants.js
+++ src/assets/Schemes/Constants.js
@@ -1,27 +1,27 @@
 
-// export const Constants = {
-//     'COORDINATOR_LOGIN': 'http://122.180.15.59/html/hospital',
-//     'VERSION_TEXT': null,
-//     'API_BASE': 'https://live.aimsindia.com/api/v3/',
-//     'APP_VERSION': '1.7',
-//     'IPD_REPORTS_URL': 'https://live.aimsindia.com/api/v3/IPDReports/',
-//     'BASE': 'https://live.aimsindia.com/',
-//     'PAYMENT_GATEWAY': 'https://live.aimsindia.com/easebuzz?productinfo=',
-//     'PAYMENT_SUCCESS': 'https://live.aimsindia.com/easebuzz/redirection',
-//     'PAYMENT_FAIL': 'https://live.aimsindia.com/easebuzz/cancel-redirection',
-// }
-
 export const Constants = {
-    'COORDINATOR_LOGIN': 'http://103.101.25.43/staging/portal/hospital',
-    'VERSION_TEXT': 'v3Test-15/12',
-    'API_BASE': 'https://appl.aimsindia.com/api/v3/',
-    'APP_VERSION': '1.6',
-    'IPD_REPORTS_URL': 'https://appl.aimsindia.com/api/v3/IPDReports/',
-    'BASE': 'https://appl.aimsindia.com/',
-    'PAYMENT_GATEWAY': 'https://appl.aimsindia.com/easebuzz?productinfo=',
-    'PAYMENT_SUCCESS': 'https://appl.aimsindia.com/easebuzz/redirection',
-    'PAYMENT_FAIL': 'https://appl.aimsindia.com/easebuzz/cancel-redirection',
+    'COORDINATOR_LOGIN': 'http://122.180.15.59/html/hospital',
+    'VERSION_TEXT': null,
+    'API_BASE': 'https://live.aimsindia.com/api/v3/',
+    'APP_VERSION': '1.7',
+    'IPD_REPORTS_URL': 'https://live.aimsindia.com/api/v3/IPDReports/',
+    'BASE': 'https://live.aimsindia.com/',
+    'PAYMENT_GATEWAY': 'https://live.aimsindia.com/easebuzz?productinfo=',
+    'PAYMENT_SUCCESS': 'https://live.aimsindia.com/easebuzz/redirection',
+    'PAYMENT_FAIL': 'https://live.aimsindia.com/easebuzz/cancel-redirection',
 }
+
+// export const Constants = {
+//     'COORDINATOR_LOGIN': 'http://103.101.25.43/staging/portal/hospital',
+//     'VERSION_TEXT': 'v3Test-15/12',
+//     'API_BASE': 'https://appl.aimsindia.com/api/v3/',
+//     'APP_VERSION': '1.6',
+//     'IPD_REPORTS_URL': 'https://appl.aimsindia.com/api/v3/IPDReports/',
+//     'BASE': 'https://appl.aimsindia.com/',
+//     'PAYMENT_GATEWAY': 'https://appl.aimsindia.com/easebuzz?productinfo=',
+//     'PAYMENT_SUCCESS': 'https://appl.aimsindia.com/easebuzz/redirection',
+//     'PAYMENT_FAIL': 'https://appl.aimsindia.com/easebuzz/cancel-redirection',
+// }
 
 export const LocalStore = {
     'APPOINTMENT_ID': ''
